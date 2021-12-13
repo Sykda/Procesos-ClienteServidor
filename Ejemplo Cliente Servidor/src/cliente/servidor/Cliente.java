7c2456d7ea32e2378d71ef3cd6f2ef5bf9e483ce
Sykda <93369567+Sykda@users.noreply.github.com>
2021-12-13 14:23:53 +0100

    commit

---
+++ Ejemplo Cliente Servidor/src/cliente/servidor/Cliente.java
@@ -0,0 +1,38 @@
+package cliente.servidor;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Cliente {
+
+	public static void main(String[] args) throws Exception {
+
+		String sentence;
+		String modifiedSentence;
+
+		// Create an input stream
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+		// Create a client socket, connect to server
+		Socket clientSocket = new Socket("127.0.0.1", 2009);
+
+		// Create an output stream attached to the socket
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+		// Create an input stream attached to socket
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		sentence = inFromUser.readLine();
+
+		// Send a line to server
+		outToServer.writeBytes(sentence + '\n');
+
+		// Read a line from server
+		modifiedSentence = inFromServer.readLine();
+
+		System.out.println("FROM SERVER: " + modifiedSentence);
+		clientSocket.close();
+
+	}
+}
---
+++ Ejemplo Cliente Servidor/src/cliente/servidor/Servidor.java
@@ -0,0 +1,46 @@
+package cliente.servidor;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor {
+
+	public static void main(String[] args) throws Exception {
+
+		String clientSentence;
+		String capitalizedSentence;
+
+		// Create welcoming socket at port 2009
+		ServerSocket welcomeSocket = new ServerSocket(2009);
+		while (true) {
+
+			// Wait, on welcoming socket for contact by client
+			Socket connectionSocket = welcomeSocket.accept();
+
+			// Create input stream, attached to socket
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+			// Create output stream, attached to socket
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+			// Read in line from socket
+			clientSentence = inFromClient.readLine();
+			capitalizedSentence = clientSentence.toUpperCase() + '\n';
+			
+			// Write out line to socket
+			outToClient.writeBytes(capitalizedSentence);
+
+			// End of while loop, bloop back and
+			// wait for another client connection
+			
+			System.out.println("SERVER Conectado al socket: "+welcomeSocket);
+			System.out.println("SERVER Cliente conectado desde: "+welcomeSocket);
+			System.out.println("SERVER Capitalized sentence: "+capitalizedSentence);
+
+		}
+	}
+}
